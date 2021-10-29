eb5168ca96bb92e538e3fb15c79de74bda798edd
sonal.chaplot <sonal.chaplot@dieboldnixdorf.com>
2021-10-29 10:29:35 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import {MatButtonModule, MatInputModule, MatCardModule} from '@angular/material'
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule, MatButtonModule, MatInputModule,FormsModule, MatCardModule]
 })
 export class AppRoutingModule { }
